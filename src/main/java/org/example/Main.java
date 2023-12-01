aefb597cb717d2e4629033efaf17e9342fccb556
aruwil1234 <aruwil1234@gmail.com>
2023-12-01 18:11:10 +0900

    11

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcme!");
+        System.out.println("Hello and welcme!");
 
         }
     }
